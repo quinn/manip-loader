469326a33b8eb18cee5b030afa8591ccb32e388b
Quinn Shanahan <quinn@honeyco.nyc>
2016-11-21 21:27:46 -0500

    webpack stuff

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+let webpack       = require('webpack')
+,   path          = require('path')
+,   ExtractPlugin = require('extract-text-webpack-plugin')
+,   output        = path.resolve('./dist')
+,   production    = process.env.NODE_ENV === 'production'
+,   imageTest     = /\.(tif?f|png|gif|jpe?g|svg|mp4|otf|ttf|cur)$/i
+,   extractCSS    = ExtractPlugin.extract({ fallbackLoader: 'style-loader', loader: ['css-loader', 'sass-loader'] })
+
+,   config = {
+	devtool: production ? 'cheap-source-map' : 'inline-source-map',
+	output:  { path: output, filename: '[name]-[hash].js' },
+	entry:   { main: 'src/index.js' },
+
+	resolve: {
+		modules: [
+			path.resolve('./src'),
+			'node_modules'
+		]
+	},
+
+	modules: {
+		loaders: [
+			{ test: /\.js$/i,   loader: 'babel-loader', exclude: /node_modules|vendor/ },
+			{ test: imageTest,  loader: 'url-loader',   query: { limit: 1000 } },
+			{ test: /\.scss$/i, loader: extractCSS      },
+		]
+	}
+}
+
+if (production)
+	config.plugins.push(
+		new webpack.optimize.AggressiveMergingPlugin(),
+		new webpack.optimize.UglifyJsPlugin({ sourceMap: true, comments: false })
+	)
+
+module.exports = config
